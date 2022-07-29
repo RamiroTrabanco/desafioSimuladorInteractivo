ef51ec40cb14327d10c14ce81752dc4b0a281984
RamiroTrabanco <trabancoramiro@gmail.com>
2022-07-29 13:54:19 -0300

    7mo commit - agregado while y corrección)

--- js/desafio.js
+++ js/desafio.js
@@ -1,67 +1,41 @@
-let productoA = false
-let productoB = false
-let productoC = false
-let descuento = 0
+debugger
+let productoA = 15000
+let productoB = 30000
+let productoC = 30000
 
-function calcularTotal() {
-    productoA = confirm("Desea adquirir este productoA?")
-    if (productoA) {
-        productoA = 15000
+let verdadero = true
+let falso = false
+
+while (verdadero) {
+    if(confirm("Deseas adquirir los productos?")) {
+        calcularTotal()
     } else {
-        productoA = 0
-    }
-    productoB = confirm("Desea adquirir este productoB?")
-    if (productoB) {
-        productoB = 30000
-    } else {
-        productoB = 0
-    }
-    productoC = confirm("Desea adquirir este productoC?")
-    if (productoC) {
-        productoC = 30000
-    } else {
-        productoC = 0
-    }
-    descuento = prompt("Ingrese su cupón de descuento:")
-    if (descuento==="cuponDel10") {
-        return (productoA + productoB + productoC) * 0.9
-    } else {
-        return productoA + productoB + productoC
+        break;
     }
 }
 
-
-/* let productoA = false
-let productoB = false
-let productoC = false
-let descuento = 0
-let valorTotal = productoA + productoB + productoC
-let totalCupon = (productoA + productoB + productoC) * 0.9
-
 function calcularTotal() {
-    productoA = confirm("Desea adquirir este productoA?")
-    if (productoA) {
-        productoA = 15000
-    } else {
-        productoA = 0
+    let total = 0
+
+    let comprarProductoA = confirm("Desea adquirir este productoA?")
+    if (comprarProductoA) {
+        total += productoA
     }
-    productoB = confirm("Desea adquirir este productoB?")
-    if (productoB) {
-        productoB = 30000
-    } else {
-        productoB = 0
+    
+    let comprarProductoB = confirm("Desea adquirir este productoB?")
+    if (comprarProductoB) {
+        total += productoB
     }
-    productoC = confirm("Desea adquirir este productoC?")
-    if (productoC) {
-        productoC = 30000
-    } else {
-        productoC = 0
+    
+    let comprarProductoC = confirm("Desea adquirir este productoC?")
+    if (comprarProductoC) {
+        total += productoC
     }
-    descuento = prompt("Ingrese su cupón de descuento:")
+    
+    let descuento = prompt("Ingrese su cupón de descuento")
     if (descuento==="cuponDel10") {
-        return totalCupon
+        console.log("El total es $", total * 0.9)
     } else {
-        return valorTotal
+        console.log("El total es $", total)
     }
 }
- */
