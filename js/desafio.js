3ebc4eb5dae1a9e564ab9cf6d5f0b70be8fc97bd
RamiroTrabanco <trabancoramiro@gmail.com>
2022-07-29 11:09:19 -0300

    primer commit

---
+++ js/desafio.js
@@ -0,0 +1,32 @@
+let productoA = false
+let productoB = false
+let productoC = false
+let valorTotal = productoA + productoB + productoC
+let totalCupon = productoA + productoB + productoC - 10%
+
+function calcularTotal() {
+    productoA = confirm("Desea adquirir este productoA?")
+    if (productoA) {
+        productoA = 15000
+    } else {
+        productoA = 0
+    }
+    productoB = confirm("Desea adquirir este productoB?")
+    if (productoB) {
+        productoB = 30000
+    } else {
+        productoB = 0
+    }
+    productoC = confirm("Desea adquirir este productoC?")
+    if (productoC) {
+        productoC = 30000
+    } else {
+        productoC = 0
+    }
+    let descuento = prompt("Ingrese su cupón de descuento")
+    if (descuento="cuponDel10") {
+        return totalCupon
+    } else {
+        return valorTotal
+    }
+}
